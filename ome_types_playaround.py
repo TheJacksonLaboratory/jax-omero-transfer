2e57c763cd56ab336c422b0c5b10817688e8411a
Erick Martins Ratamero <erickmartins@gmx.net>
2021-12-28 13:29:12 -0500

    ROI stuff mostly done (except for Ellipses)

--- ome_types_playaround.py
+++ ome_types_playaround.py
@@ -4,8 +4,10 @@ from ome_types.model import Dataset, DatasetRef
 from ome_types.model import Image, ImageRef, Pixels
 from ome_types.model import TagAnnotation, MapAnnotation, ROI
 from ome_types.model import AnnotationRef, ROIRef, Map
+from ome_types.model import Point, Line, Rectangle, Ellipse, Polygon, Polyline
 from ome_types.model.map import M
 from omero.model import TagAnnotationI, MapAnnotationI
+from omero.model import PointI, LineI, RectangleI, EllipseI, PolygonI, PolylineI
 import ezomero
 import argparse
 
@@ -39,15 +41,188 @@ def create_image_and_ref(**kwargs):
     img_ref = ImageRef(id=img.id)
     return img, img_ref
 
-def create_tag_and_ref(ome, **kwargs):
+def create_tag_and_ref(**kwargs):
     tag = TagAnnotation(**kwargs)
     tagref = AnnotationRef(id=tag.id)
     return tag, tagref
 
-def create_kv_and_ref(ome, **kwargs):
+def create_kv_and_ref(**kwargs):
     kv = MapAnnotation(**kwargs)
     kvref = AnnotationRef(id=kv.id)
     return kv, kvref
+
+def create_roi_and_ref(**kwargs):
+    print('kwargs:', kwargs)
+    roi = ROI(**kwargs)
+    print('roi:', roi)
+    roiref = ROIRef(id=roi.id)
+    return roi, roiref
+
+def create_point(shape):
+    args = {'id': shape.getId().val, 'x': shape.getX().val, 'y': shape.getY().val}
+    if shape.getTextValue() is not None:
+        args['text'] = shape.getTextValue().val
+    if shape.getTheC() is not None:
+        args['the_c'] = shape.getTheC().val
+    if shape.getTheZ() is not None:
+        args['the_z'] = shape.getTheZ().val
+    if shape.getTheT() is not None:
+        args['the_t'] = shape.getTheT().val
+    if shape.getFillColor() is not None:
+        args['fill_color'] = shape.getFillColor().val
+    if shape.getLocked() is not None:
+        args['locked'] = shape.getLocked().val
+    if shape.getStrokeColor() is not None:
+        args['stroke_color'] = shape.getStrokeColor().val
+    pt = Point(**args)
+    return pt
+
+def create_line(shape):
+    args = {'id': shape.getId().val, 'x1': shape.getX1().val, 'y1': shape.getY1().val,
+            'x2': shape.getX2().val, 'y2': shape.getY2().val}
+    if shape.getTextValue() is not None:
+        args['text'] = shape.getTextValue().val
+    if shape.getTheC() is not None:
+        args['the_c'] = shape.getTheC().val
+    if shape.getTheZ() is not None:
+        args['the_z'] = shape.getTheZ().val
+    if shape.getTheT() is not None:
+        args['the_t'] = shape.getTheT().val
+    if shape.getFillColor() is not None:
+        args['fill_color'] = shape.getFillColor().val
+    if shape.getLocked() is not None:
+        args['locked'] = shape.getLocked().val
+    if shape.getStrokeColor() is not None:
+        args['stroke_color'] = shape.getStrokeColor().val
+    ln = Line(**args)
+    return ln
+
+def create_rectangle(shape):
+    args = {'id': shape.getId().val, 'x': shape.getX().val, 'y': shape.getY().val,
+            'height': shape.getHeight().val, 'width': shape.getWidth().val}
+    if shape.getTextValue() is not None:
+        args['text'] = shape.getTextValue().val
+    if shape.getTheC() is not None:
+        args['the_c'] = shape.getTheC().val
+    if shape.getTheZ() is not None:
+        args['the_z'] = shape.getTheZ().val
+    if shape.getTheT() is not None:
+        args['the_t'] = shape.getTheT().val
+    if shape.getFillColor() is not None:
+        args['fill_color'] = shape.getFillColor().val
+    if shape.getLocked() is not None:
+        args['locked'] = shape.getLocked().val
+    if shape.getStrokeColor() is not None:
+        args['stroke_color'] = shape.getStrokeColor().val
+    rec = Rectangle(**args)
+    return rec
+
+def create_ellipse(shape):
+    args = {'id': shape.getId().val, 'x': shape.getX().val, 'y': shape.getY().val,
+            'radius_x': shape.getRadiusX().val, 'radius_y': shape.getRadiusY().val}
+    if shape.getTextValue() is not None:
+        args['text'] = shape.getTextValue().val
+    if shape.getTheC() is not None:
+        args['the_c'] = shape.getTheC().val
+    if shape.getTheZ() is not None:
+        args['the_z'] = shape.getTheZ().val
+    if shape.getTheT() is not None:
+        args['the_t'] = shape.getTheT().val
+    if shape.getFillColor() is not None:
+        args['fill_color'] = shape.getFillColor().val
+    if shape.getLocked() is not None:
+        args['locked'] = shape.getLocked().val
+    if shape.getStrokeColor() is not None:
+        args['stroke_color'] = shape.getStrokeColor().val
+    ell = Ellipse(**args)
+    return ell
+
+def create_polygon(shape):
+    args = {'id': shape.getId().val, 'points': shape.getPoints().val}
+    if shape.getTextValue() is not None:
+        args['text'] = shape.getTextValue().val
+    if shape.getTheC() is not None:
+        args['the_c'] = shape.getTheC().val
+    if shape.getTheZ() is not None:
+        args['the_z'] = shape.getTheZ().val
+    if shape.getTheT() is not None:
+        args['the_t'] = shape.getTheT().val
+    if shape.getFillColor() is not None:
+        args['fill_color'] = shape.getFillColor().val
+    if shape.getLocked() is not None:
+        args['locked'] = shape.getLocked().val
+    if shape.getStrokeColor() is not None:
+        args['stroke_color'] = shape.getStrokeColor().val
+    pol = Polygon(**args)
+    return pol
+
+def create_polyline(shape):
+    args = {'id': shape.getId().val, 'points': shape.getPoints().val}
+    if shape.getTextValue() is not None:
+        args['text'] = shape.getTextValue().val
+    if shape.getTheC() is not None:
+        args['the_c'] = shape.getTheC().val
+    if shape.getTheZ() is not None:
+        args['the_z'] = shape.getTheZ().val
+    if shape.getTheT() is not None:
+        args['the_t'] = shape.getTheT().val
+    if shape.getFillColor() is not None:
+        args['fill_color'] = shape.getFillColor().val
+    if shape.getLocked() is not None:
+        args['locked'] = shape.getLocked().val
+    if shape.getStrokeColor() is not None:
+        args['stroke_color'] = shape.getStrokeColor().val
+    pline = Polyline(**args)
+    return pline
+
+def create_shapes(roi):
+    shapes = []
+    for s in roi.iterateShapes():
+        if isinstance(s, PointI):
+            p = create_point(s)
+            shapes.append(p)
+        if isinstance(s, LineI):
+            l = create_line(s)
+            shapes.append(l)
+        if isinstance(s, RectangleI):
+            r = create_rectangle(s)
+            shapes.append(r)
+        if isinstance(s, EllipseI):
+            e = create_ellipse(s)
+            shapes.append(e)
+        if isinstance(s, PolygonI):
+            pol = create_polygon(s)
+            shapes.append(pol)
+        if isinstance(s, PolylineI):
+            pline = create_polyline(s)
+            shapes.append(pline)
+    return shapes
+
+def populate_roi(obj, roi_obj, ome, conn):
+    id = obj.getId().getValue()
+    name = obj.getName()
+    if name is not None:
+        name = name.getValue()
+    desc = obj.getDescription()
+    if desc is not None:
+        desc = desc.getValue()
+    shapes = create_shapes(obj)
+    roi, roi_ref = create_roi_and_ref(id=id, name=name, description=desc, union=shapes)
+    for ann in roi_obj.listAnnotations():
+        if ann.OMERO_TYPE == TagAnnotationI:
+            tag, ref = create_tag_and_ref(id=ann.getId(), value=ann.getTextValue())
+            if tag not in ome.structured_annotations:
+                ome.structured_annotations.append(tag)
+            roi.annotation_ref.append(ref)
+        if ann.OMERO_TYPE == MapAnnotationI:
+            kv, ref = create_kv_and_ref(id=ann.getId(), value=Map(m=[M(k=_key, value=str(_value)) for _key, _value in ann.getMapValueAsMap().items()]))
+            # print(kv.value.m[0].k) # this is how you retrieve the key for the first kv pair in this annotation
+            if kv not in ome.structured_annotations:
+                ome.structured_annotations.append(kv)
+            roi.annotation_ref.append(ref)
+    if roi not in ome.rois:
+        ome.rois.append(roi)
+    return roi_ref
 
 def populate_image(obj, ome, conn):
     id = obj.getId()
@@ -57,16 +232,23 @@ def populate_image(obj, ome, conn):
     img, img_ref = create_image_and_ref(id=id, name=name, description=desc, pixels=pix)
     for ann in obj.listAnnotations():
         if ann.OMERO_TYPE == TagAnnotationI:
-            tag, ref = create_tag_and_ref(ome, id=ann.getId(), value=ann.getTextValue())
+            tag, ref = create_tag_and_ref(id=ann.getId(), value=ann.getTextValue())
             if tag not in ome.structured_annotations:
                 ome.structured_annotations.append(tag)
             img.annotation_ref.append(ref)
         if ann.OMERO_TYPE == MapAnnotationI:
-            kv, ref = create_kv_and_ref(ome, id=ann.getId(), value=Map(m=[M(k=_key, value=str(_value)) for _key, _value in ann.getMapValueAsMap().items()]))
-            print(kv.value.m[0].k) # this is how you retrieve the key for the first kv pair in this annotation
+            kv, ref = create_kv_and_ref(id=ann.getId(), value=Map(m=[M(k=_key, value=str(_value)) for _key, _value in ann.getMapValueAsMap().items()]))
+            # print(kv.value.m[0].k) # this is how you retrieve the key for the first kv pair in this annotation
             if kv not in ome.structured_annotations:
                 ome.structured_annotations.append(kv)
             img.annotation_ref.append(ref)
+    roi_service = conn.getRoiService()
+    rois = roi_service.findByImage(id, None).rois
+    for roi in rois:
+        roi_obj = conn.getObject('Roi', roi.getId().getValue())
+        roi_ref = populate_roi(roi, roi_obj, ome, conn)
+        img.roi_ref.append(roi_ref)
+            
     if img not in ome.images:
         ome.images.append(img)
     return img_ref
@@ -79,13 +261,13 @@ def populate_dataset(obj, ome, conn):
     ds, ds_ref = create_dataset_and_ref(id=id, name=name, description=desc)
     for ann in obj.listAnnotations():
         if ann.OMERO_TYPE == TagAnnotationI:
-            tag, ref = create_tag_and_ref(ome, id=ann.getId(), value=ann.getTextValue())
+            tag, ref = create_tag_and_ref(id=ann.getId(), value=ann.getTextValue())
             if tag not in ome.structured_annotations:
                 ome.structured_annotations.append(tag)
             ds.annotation_ref.append(ref)
         if ann.OMERO_TYPE == MapAnnotationI:
-            kv, ref = create_kv_and_ref(ome, id=ann.getId(), value=Map(m=[M(k=_key, value=str(_value)) for _key, _value in ann.getMapValueAsMap().items()]))
-            print(kv.value.m[0].k) # this is how you retrieve the key for the first kv pair in this annotation
+            kv, ref = create_kv_and_ref(id=ann.getId(), value=Map(m=[M(k=_key, value=str(_value)) for _key, _value in ann.getMapValueAsMap().items()]))
+            # print(kv.value.m[0].k) # this is how you retrieve the key for the first kv pair in this annotation
             if kv not in ome.structured_annotations:
                 ome.structured_annotations.append(kv)
             ds.annotation_ref.append(ref)
@@ -105,13 +287,13 @@ def populate_project(obj, ome, conn):
     test_proj, _ = create_proj_and_ref(id=id, name=name, description=desc)
     for ann in obj.listAnnotations():
         if ann.OMERO_TYPE == TagAnnotationI:
-            tag, ref = create_tag_and_ref(ome, id=ann.getId(), value=ann.getTextValue())
+            tag, ref = create_tag_and_ref(id=ann.getId(), value=ann.getTextValue())
             if tag not in ome.structured_annotations:
                 ome.structured_annotations.append(tag)
             test_proj.annotation_ref.append(ref)
         if ann.OMERO_TYPE == MapAnnotationI:
-            kv, ref = create_kv_and_ref(ome, id=ann.getId(), value=Map(m=[M(k=_key, value=str(_value)) for _key, _value in ann.getMapValueAsMap().items()]))
-            print(kv.value.m[0].k) # this is how you retrieve the key for the first kv pair in this annotation
+            kv, ref = create_kv_and_ref(id=ann.getId(), value=Map(m=[M(k=_key, value=str(_value)) for _key, _value in ann.getMapValueAsMap().items()]))
+            # print(kv.value.m[0].k) # this is how you retrieve the key for the first kv pair in this annotation
             if kv not in ome.structured_annotations:
                 ome.structured_annotations.append(kv)
             test_proj.annotation_ref.append(ref)
